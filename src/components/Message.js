8f360f6c12bb681f6100936449fe2d1b37a2f347
Casey Kelly <casey.kelly@dish.com>
2018-03-01 11:22:49 -0700

    message stars can be toggled

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Message = ({ subject, read, selected, labels, starred }) => {
-  const messageStyle = `row message ${read ? 'read' : 'unread'} ${
-    selected ? 'selected' : ''
-  }`;
-  const checkboxStyle = selected ? 'checked' : '';
-  const starStyle = starred ? 'star fa fa-star' : 'star fa fa-star-o';
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messageStyle: `row message ${this.props.read ? 'read' : 'unread'} ${
+        this.props.selected ? 'selected' : ''
+      }`,
+      checkboxStyle: this.props.selected ? 'checked' : '',
+      starStyle: this.props.starred ? 'star fa fa-star' : 'star fa fa-star-o'
+    };
+  }
 
-  return (
-    <div className={messageStyle}>
-      <div className="col-xs-1">
-        <div className="row">
-          <div className="col-xs-2">
-            <input type="checkbox" defaultChecked={checkboxStyle} />
-          </div>
-          <div className="col-xs-2">
-            <i className={starStyle} />
+  toggleStarred = () => {
+    const toggleStyle =
+      this.state.starStyle === 'star fa fa-star'
+        ? 'star fa fa-star-o'
+        : 'star fa fa-star';
+    this.setState({ starStyle: toggleStyle });
+  };
+
+  render() {
+    return (
+      <div className={this.state.messageStyle}>
+        <div className="col-xs-1">
+          <div className="row">
+            <div className="col-xs-2">
+              <input
+                type="checkbox"
+                defaultChecked={this.state.checkboxStyle}
+              />
+            </div>
+            <div className="col-xs-2">
+              <i
+                className={this.state.starStyle}
+                onClick={this.toggleStarred}
+              />
+            </div>
           </div>
         </div>
+        <div className="col-xs-11">
+          {this.props.labels.map(label => (
+            <span key={label} className="label label-warning">
+              {label}
+            </span>
+          ))}
+          <a href="#">{this.props.subject}</a>
+        </div>
       </div>
-      <div className="col-xs-11">
-        {labels.map(label => (
-          <span key={label} className="label label-warning">
-            {label}
-          </span>
-        ))}
-        <a href="#">{subject}</a>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Message;
