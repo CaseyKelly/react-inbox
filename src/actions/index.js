6203a5c418f4e6cba010c4807735c1bfa637221f
Casey Kelly <casey.kelly@dish.com>
2018-03-07 11:07:57 -0700

    create action creators

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const MESSAGES_RECEIVED = 'MESSAGES_RECEIVED';
+export function fetchMessages() {
+  return async dispatch => {
+    const response = await fetch(`http://localhost:8082/api/messages`);
+    const json = await response.json();
+    dispatch({
+      type: MESSAGES_RECEIVED,
+      messages: json._embedded.messages
+    });
+  };
+}
